96d87115ed1a46b1a601199b729c9ce5ea7a6f92
Nick <n.sychevskyi@gmail.com>
2021-01-18 22:49:45 +0200

    Create getFighterDetails method in FighterService class. Create getFighterInfo function. Create FighterPreview functionality and UI.

--- src/javascript/components/fighterPreview.js
+++ src/javascript/components/fighterPreview.js
@@ -7,7 +7,43 @@ export function createFighterPreview(fighter, position) {
     className: `fighter-preview___root ${positionClassName}`,
   });
 
-  // todo: show fighter info (image, name, health, etc.)
+  if (fighter) {
+    const imgElement = createFighterImage(fighter);
+    const infoElement = createElement({
+      tagName: 'ul',
+      className: 'fighter-preview__features',
+    });
+
+    const {_id, source, ...fighterOthersDetails} = fighter;
+
+    Object.keys(fighterOthersDetails).forEach(feature => {
+      const featureElement = createElement({
+        tagName: 'li',
+        className: `fighter-preview__features-item`,
+      });
+
+      const featureElementProperty = createElement({
+        tagName: 'span',
+        className: `fighter-preview__features-property`,
+      });
+
+      featureElementProperty.innerText = `${feature}: `;
+
+      const featureElementValue = createElement({
+        tagName: 'span',
+        className: `fighter-preview__features-value`,
+      });
+
+      featureElementValue.innerText = fighterOthersDetails[feature];
+
+      featureElement.append(featureElementProperty);
+      featureElement.append(featureElementValue);
+      infoElement.append(featureElement);
+    });
+
+    fighterElement.append(infoElement);
+    fighterElement.append(imgElement);
+  }
 
   return fighterElement;
 }
